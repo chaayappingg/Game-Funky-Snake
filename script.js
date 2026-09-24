cafd7ee62403aea87d73eea7134d6c35a58802c1
chaaa <andiazizahk@gmail.com>
2026-09-24 15:00:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function inisialisasiGame() {
     partikelArray = [];
     acakPosisiUmpan();
     if (gameLoopInterval) clearInterval(gameLoopInterval);
-    gameLoopInterval = setInterval(updateDanRender, 180);
+    gameLoopInterval = setInterval(updateDanRender, 200);
     gameBerjalan = true;
     btnRestart.innerText = "Reset Kontrol 🎮";
 }
